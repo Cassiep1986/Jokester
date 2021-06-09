a476dc126aed5f01f9b18b49c975014734b54b59
seacrest <cassiep1986@gmail.com>
2021-06-09 14:07:55 -0700

    added second api

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-var jokeList = document.querySelector('ul');
-var Button = document.getElementById('button');
+var jokeList = document.querySelector("ul");
+var Button = document.getElementById("button");
 
-function getApi() { 
-  var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+function getjokeApi() {
+    var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        var listItem = document.createElement('li');
+        console.log(data)
+      var listItem = document.createElement("li");
 
-        listItem.textContent = data.setup;
+      listItem.textContent = data.setup;
 
-        jokeList.appendChild(listItem);
+    //   jokeList.appendChild(listItem);
     });
 }
 
-Button.addEventListener('click', getApi);
+fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
+        method: 'get',
+        headers: { 'x-api-key': 'BvCHj7SQIc4n' },
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+
+// Button.addEventListener('click', getjokeApi);
+getjokeApi();
