61f5fd3fb9011b43e565b8813ad5f50751850e94
seacrest <cassiep1986@gmail.com>
2021-06-10 16:08:47 -0700

    merging conflicts

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var jokeList = $("#ul");
 var Button = $("#button");
 var modalButton = $("#aboutUs");
-
 const saveButton = $("#saveBtn");
 const savedContent = $("#savedFavorites");
 let jokeDay = $("#jokeOfTheDay")
 
+//click on save btn
+// saved to local storage
+//and display multiple saves to our favorites category.
+//make saved favorites clickable items on the page for display later.
+function creatSave(){
+  const saveFavs = $("<li>");
+
+
+}
 function getjokeApi() {
   var requestUrl =
     "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
@@ -29,66 +37,78 @@ function getjokeApi() {
 
       // }
 
+$(saveButton).on("click", creatSave)
       // $(saveButton).on("click", creatSave)
-
-
-      // var jokeList = document.querySelector('ul')
-      // var Button = document.getElementById('button');
 
   
   
   // var jokeList = document.querySelector('ul')
   // var Button = document.getElementById('button');
+      // var jokeList = document.querySelector('ul')
+      // var Button = document.getElementById('button');
 
+// function getApi() {
+//   var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
       // function getApi() {
       //   var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
 
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
       //   fetch(requestUrl)
       //     .then(function (response) {
       //       return response.json();
       //     })
       //     .then(function (data) {
 
+//         var listItem = document.createElement('li');
       //         var listItem = document.createElement('li');
 
+//         listItem.textContent = data.setup;
       //         listItem.textContent = data.setup;
 
+//         // jokeList.appendChild(listItem);
+//     });
+// }
       //         // jokeList.appendChild(listItem);
       //     });
       // }
 
+// Button.addEventListener('click', getApi);
       // Button.addEventListener('click', getApi);
-=======
-var container2 = $("#container2");
 
-function getjokeApi() {
-  var requestUrl =
-  "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
-  
-  fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    var listItem = document.createElement("p");
-    data.jokes.forEach(element => {
-      listItem.textContent = data.jokes;
-      container2.append(listItem);
-      
-    });
-      
-    
-  });
-}
-getjokeApi();
+fetch(
+  'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
+  {
+    method: "get",
+    headers: { "x-api-key": "dBzuyLCxJ06r" },
+  }
+)
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+      // Button.addEventListener('click', getjokeApi);
+      // getjokeApi();
 
-
-
+var getWeather = function (city) {
+  var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
       modalButton.on("click", function (event) {
         $("#aboutUsModal").addClass("is-active");
       });
 
+  return fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          return displayWeather(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to gather weather data!");
       $(".modal-close").on("click", function (e) {
         $("#aboutUsModal").removeClass("is-active");
       });
@@ -104,25 +124,10 @@ getjokeApi();
         dropdown.classList.toggle("is-active");
       });
     });
-
-}
-
 };
 
-const saveButton = $("#saveBtn");
-const savedContent = $("#savedFavorites");
-
-//click on save btn
-// saved to local storage
-//and display multiple saves to our favorites category.
-//make saved favorites clickable items on the page for display later.
-function creatSave(){
-  const saveFavs = $("<li>"); 
-}
-
-$(saveButton).on("click", creatSave)
-
-
+// Button.addEventListener('click', getjokeApi);
+// getjokeApi();
 
 modalButton.on("click", function (event) {
   $("#aboutUsModal").addClass("is-active");
@@ -131,14 +136,6 @@ modalButton.on("click", function (event) {
 $(".modal-close").on("click", function (e) {
   $("#aboutUsModal").removeClass("is-active");
 });
-
-    $("#aboutUsModal").removeClass("is-active");
-
-
-
-$(".modal-background").on("click", function (e) {
-    $("#aboutUsModal").removeClass("is-active");
-})
 
 var dropdown = document.querySelector(".dropdown");
 
@@ -150,29 +147,3 @@ dropdown.addEventListener("click", function (event) {
   //classList.toggle - it toggles between adding and removing a class name from an element
   dropdown.classList.toggle("is-active");
 });
-
-// fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
-//         method: 'get',
-//         headers: { 'x-api-key': 'dBzuyLCxJ06r' },
-//     })
-//     .then(res => res.json())
-//     .then(json => console.log(json));
-
-//     var getWeather = function (city) {
-//       var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
-    
-//       return fetch(apiUrl)
-//         .then(function (response) {
-//           if (response.ok) {
-//             response.json().then(function (data) {
-//               return displayWeather(data);
-//             });
-//           } else {
-//             alert("Error: " + response.statusText);
-//           }
-//         })
-//         .catch(function (error) {
-//           alert("Unable to gather weather data!");
-//         });
-//     };
-
