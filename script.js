8d4d5bcc3b53a00d18fdbaf6b3a9e629fd301644
seacrest <cassiep1986@gmail.com>
2021-06-10 12:50:38 -0700

    adding api info

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 var jokeList = document.querySelector("ul");
 var Button = document.getElementById("button");
+var Button = document.getElementById("container2");
 
 function getjokeApi() {
-    var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
-
+  var requestUrl =
+    "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data)
-      var listItem = document.createElement("li");
-
-      listItem.textContent = data.setup;
-
-    //   jokeList.appendChild(listItem);
+      console.log(data);
+      var listItem = document.createElement("p");
+      listItem.textContent = data.jokes.joke;
+      container2.append(listItem);
     });
 }
 
-fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
-        method: 'get',
-        headers: { 'x-api-key': 'BvCHj7SQIc4n' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-
+fetch(
+  'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
+  {
+    method: "get",
+    headers: { "x-api-key": "BvCHj7SQIc4n" },
+  }
+)
+  .then((res) => res.json())
+  .then((json) => console.log(json));
 
 // Button.addEventListener('click', getjokeApi);
 getjokeApi();
