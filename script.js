e9cb8f7c96565bdd15e383d055f595160fc18d17
Adam Burch <rewop360@gmail.com>
2021-06-10 11:15:10 -0700

    dropdown working

--- script.js
+++ script.js
@@ -5,27 +5,48 @@ function getjokeApi() {
     var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
 
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-      var listItem = document.createElement("li");
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var listItem = document.createElement("li");
 
-      listItem.textContent = data.setup;
+            listItem.textContent = data.setup;
 
-    //   jokeList.appendChild(listItem);
-    });
+            //   jokeList.appendChild(listItem);
+        });
 }
 
 fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
-        method: 'get',
-        headers: { 'x-api-key': 'BvCHj7SQIc4n' },
-    })
+    method: 'get',
+    headers: { 'x-api-key': 'BvCHj7SQIc4n' },
+})
     .then(res => res.json())
     .then(json => console.log(json));
 
 
 // Button.addEventListener('click', getjokeApi);
 getjokeApi();
+
+
+
+
+
+
+
+debugger;
+
+var dropdown = document.querySelector('.dropdown');
+    console.log("outside event");
+
+//addEventListener - attaches an event handler to the specified element.
+dropdown.addEventListener('click', function (event) {
+    console.log("event fired");
+    //event.stopPropagation() - it stops the bubbling of an event to parent elements, by preventing parent event handlers from being executed
+    event.stopPropagation();
+
+    //classList.toggle - it toggles between adding and removing a class name from an element
+    dropdown.classList.toggle('is-active');
+});
