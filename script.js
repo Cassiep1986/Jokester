f912bcbfa7600470862903cc37c228dbcfd38aa5
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-06-10 13:50:12 -0700

    comment out function call

--- script.js
+++ script.js
@@ -2,77 +2,71 @@ var jokeList = $("#ul");
 var Button = $("#button");
 var modalButton = $("#aboutUs");
 
-  // function getApi() {
-  //   var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+// function getApi() {
+//   var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
 
-  //   fetch(requestUrl)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-      
-  //         var listItem = document.createElement('li');
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
 
-  //         listItem.textContent = data.setup;
-          
+//         var listItem = document.createElement('li');
 
-  //         // jokeList.appendChild(listItem);
-  //     });
-  // }
+//         listItem.textContent = data.setup;
 
+//         // jokeList.appendChild(listItem);
+//     });
+// }
 
 // Button.addEventListener('click', getApi);
 
-fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
-        method: 'get',
-        headers: { 'x-api-key': 'dBzuyLCxJ06r' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
+fetch(
+  'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
+  {
+    method: "get",
+    headers: { "x-api-key": "dBzuyLCxJ06r" },
+  }
+)
+  .then((res) => res.json())
+  .then((json) => console.log(json));
 
-    var getWeather = function (city) {
-      var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
-    
-      return fetch(apiUrl)
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              return displayWeather(data);
-            });
-          } else {
-            alert("Error: " + response.statusText);
-          }
-        })
-        .catch(function (error) {
-          alert("Unable to gather weather data!");
+var getWeather = function (city) {
+  var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
+
+  return fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          return displayWeather(data);
         });
-    };
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to gather weather data!");
+    });
+};
 
 // Button.addEventListener('click', getjokeApi);
-getjokeApi();
-
-
-
-
-
+// getjokeApi();
 
 modalButton.on("click", function (event) {
-    $("#aboutUsModal").addClass("is-active");
-
+  $("#aboutUsModal").addClass("is-active");
 });
 
 $(".modal-close").on("click", function (e) {
-    $("#aboutUsModal").removeClass("is-active");
+  $("#aboutUsModal").removeClass("is-active");
+});
 
-})
-
-var dropdown = document.querySelector('.dropdown');
+var dropdown = document.querySelector(".dropdown");
 
 //addEventListener - attaches an event handler to the specified element.
-dropdown.addEventListener('click', function (event) {
-    //event.stopPropagation() - it stops the bubbling of an event to parent elements, by preventing parent event handlers from being executed
-    event.stopPropagation();
+dropdown.addEventListener("click", function (event) {
+  //event.stopPropagation() - it stops the bubbling of an event to parent elements, by preventing parent event handlers from being executed
+  event.stopPropagation();
 
-    //classList.toggle - it toggles between adding and removing a class name from an element
-    dropdown.classList.toggle('is-active');
+  //classList.toggle - it toggles between adding and removing a class name from an element
+  dropdown.classList.toggle("is-active");
 });
