1c461272d4b697142202380231ba91fe2459e4e8
seacrest <cassiep1986@gmail.com>
2021-06-10 14:47:50 -0700

    troubleshooting fetch

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ var container2 = $("#container2");
 
 function getjokeApi() {
   var requestUrl =
-    "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
-
+  "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
+  
   fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      var listItem = document.createElement("p");
-      listItem.textContent = data.jokes[0].joke;
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var listItem = document.createElement("p");
+    data.jokes.forEach(element => {
+      listItem.textContent = data.jokes;
       container2.append(listItem);
+      
     });
+      
+    
+  });
 }
+getjokeApi();
 
 fetch(
   'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
@@ -28,6 +34,7 @@ fetch(
 )
   .then((res) => res.json())
   .then((json) => console.log(json));
+
 const saveButton = $("#saveBtn");
 const savedContent = $("#savedFavorites");
 
