0a4841334687aef5505088bbabb4a145f8286edd
Joseph Amador <kirito.1986.714@gmail.com>
2021-06-11 10:39:51 -0700

    cleaned up java

--- script.js
+++ script.js
@@ -3,44 +3,28 @@ var Button = $("#button");
 var modalButton = $("#aboutUs");
 const saveButton = $("#saveBtn");
 const savedContent = $("#savedFavorites");
+const jokeDay = $("#jokeOfDay");
 
 //click on save btn
 // saved to local storage
 //and display multiple saves to our favorites category.
 //make saved favorites clickable items on the page for display later.
-function creatSave() {
-  const saveFavs = $("<li>");
-}
 
-$(saveButton).on("click", creatSave);
-
-fetch(
-  'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
-  {
-    method: "get",
-    headers: { "x-api-key": "dBzuyLCxJ06r" },
-  }
-)
-  .then((res) => res.json())
-  .then((json) => console.log(json));
-
-var getWeather = function (city) {
-  var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
-
-  return fetch(apiUrl)
+function getJokeApi() {
+  const requestUrl =
+    "https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-1&amount=2";
+  fetch(requestUrl)
     .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          return displayWeather(data);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
+      return response.json();
     })
-    .catch(function (error) {
-      alert("Unable to gather weather data!");
+    .then(function (data) {
+      const listItem = $("<p>");
+      data.jokes.forEach((data) => {
+        listItem.html = data.jokes;
+        jokeDay.append(listItem);
+      });
     });
-};
+}
 
 modalButton.on("click", function (event) {
   $("#aboutUsModal").addClass("is-active");
