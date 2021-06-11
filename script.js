f8c9eced5682067d1fa42ec19475f802ff20bbbc
seacrest <cassiep1986@gmail.com>
2021-06-11 10:11:27 -0700

    cleaning up code

--- script.js
+++ script.js
@@ -8,39 +8,11 @@ const savedContent = $("#savedFavorites");
 // saved to local storage
 //and display multiple saves to our favorites category.
 //make saved favorites clickable items on the page for display later.
-function creatSave(){
+function creatSave() {
   const saveFavs = $("<li>");
- 
- 
 }
 
-
-
-$(saveButton).on("click", creatSave)
-
-  
-  
-  // var jokeList = document.querySelector('ul')
-  // var Button = document.getElementById('button');
-
-// function getApi() {
-//   var requestUrl = 'https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
-
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-
-//         var listItem = document.createElement('li');
-
-//         listItem.textContent = data.setup;
-
-//         // jokeList.appendChild(listItem);
-//     });
-// }
-
-// Button.addEventListener('click', getApi);
+$(saveButton).on("click", creatSave);
 
 fetch(
   'https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable',
@@ -70,9 +42,6 @@ var getWeather = function (city) {
     });
 };
 
-// Button.addEventListener('click', getjokeApi);
-// getjokeApi();
-
 modalButton.on("click", function (event) {
   $("#aboutUsModal").addClass("is-active");
 });
@@ -82,8 +51,8 @@ $(".modal-close").on("click", function (e) {
 });
 
 $(".modal-background").on("click", function (e) {
-    $("#aboutUsModal").removeClass("is-active");
-})
+  $("#aboutUsModal").removeClass("is-active");
+});
 
 var dropdown = document.querySelector(".dropdown");
 
@@ -95,27 +64,3 @@ dropdown.addEventListener("click", function (event) {
   //classList.toggle - it toggles between adding and removing a class name from an element
   dropdown.classList.toggle("is-active");
 });
-// fetch('https://api.pgamerx.com/v3/ai/response?message=encodeURIComponent("Hello")&type=stable', {
-//         method: 'get',
-//         headers: { 'x-api-key': 'dBzuyLCxJ06r' },
-//     })
-//     .then(res => res.json())
-//     .then(json => console.log(json));
-
-//     var getWeather = function (city) {
-//       var apiUrl = `https://api.openweathermap$.org/data/2.5/weather?q=+${city}&units=imperial&APPID=904755abfca69992b8a848481a87baea`;
-    
-//       return fetch(apiUrl)
-//         .then(function (response) {
-//           if (response.ok) {
-//             response.json().then(function (data) {
-//               return displayWeather(data);
-//             });
-//           } else {
-//             alert("Error: " + response.statusText);
-//           }
-//         })
-//         .catch(function (error) {
-//           alert("Unable to gather weather data!");
-//         });
-//     };
