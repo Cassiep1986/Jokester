c3d86aa64432dfbe3a4de5f83bb8e17b7312e6a8
seacrest <cassiep1986@gmail.com>
2021-06-12 17:16:31 -0700

    cleanup

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var Button = $("#button");
 var modalButton = $("#aboutUs");
 var saveButton = $("#saveBtn");
 const savedContent = $("#savedFavorites");
+var savedJokesArray = localStorage.getItem("jokes1 save") ? localStorage.getItem("jokes1 save") :[];
 
 //click on save btn
 // saved to local storage
@@ -10,6 +11,7 @@ const savedContent = $("#savedFavorites");
 //make saved favorites clickable items on the page for display later.
 
 jokeOftheDay();
+getSavedValue();
 
 function jokeOftheDay() {
   const requestUrl =
@@ -23,20 +25,23 @@ function jokeOftheDay() {
       console.log(data);
     });
 }
-saveButton.on("click", function (event) {
-  var joke = $("#jokeOfDay").text();
-  console.log(joke);
-<<<<<<< HEAD
-<<<<<<< HEAD
-  localStorage.setItem("joke1 save", joke);
+
+saveButton.on("click", function (e){
+  var joke =$("#jokeOfDay").text()
+
+  localStorage.setItem('joke1 save', joke);
   appendjoke();
-=======
-  localStorage.setItem('joke1 save', joke);
->>>>>>> parent of bd2e9ed (saved jokes to side of page)
-=======
-  localStorage.setItem('joke1 save', joke);
->>>>>>> parent of fc771ff (cleaning)
 });
+
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue  (){
+  var joke1 = localStorage.getItem("joke1 save");
+  if (!localStorage.getItem("joke1 save", joke1)) {
+    return $("<li>" + joke1 + "</li>").appendTo(savedContent);;// You can change this to your defualt value. 
+  }
+
+  return $("<li>" + joke1 + "</li>").appendTo(savedContent);;
+}
 
 modalButton.on("click", function (event) {
   $("#aboutUsModal").addClass("is-active");
@@ -53,35 +58,38 @@ $(".modal-background").on("click", function (e) {
 var dropdown = document.querySelector(".dropdown");
 
 //addEventListener - attaches an event handler to the specified element.
+
 dropdown.addEventListener("click", function (event) {
   //event.stopPropagation() - it stops the bubbling of an event to parent elements, by preventing parent event handlers from being executed
+
   event.stopPropagation();
 
   //classList.toggle - it toggles between adding and removing a class name from an element
   dropdown.classList.toggle("is-active");
   var category = event.target.text;
-  console.log(category);
-  for (let i = 0; i < 10; i++) {
-    fetch(
-      `https://v2.jokeapi.dev/joke/${category}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=1`
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        $("#jokesMain").text(data.joke);
-      });
-  }
+  
+  fetch(
+    `https://v2.jokeapi.dev/joke/${category}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //itirate through joeks array creating li's and appending to jokesMain, with the joke text in li.
+      data.jokes.forEach((e) => {
+        $("#jokesMain").append(
+          $(`<li>${e.joke}<button class="jokeSaver">Save</button></li> <br>`)
+        );
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-
+      $(".jokeSaver").on("click", function (event) {
+        console.log("jokeSaverClick");
+        var joke = $(this).parent().text();
+        console.log(joke);
+        localStorage.setItem("joke1 save", joke);
+        appendjoke();
+      });
+    });
 function appendjoke() {
-  var joke1 = localStorage.getItem("joke1 save");
-  $("<li>" + joke1 + "</li>").appendTo(savedContent);
-}
-=======
->>>>>>> parent of bd2e9ed (saved jokes to side of page)
-=======
->>>>>>> parent of fc771ff (cleaning)
+var joke1 = localStorage.getItem('joke1 save');
+$("<li>"+ joke1+"</li>").appendTo(savedContent);
+ }});
